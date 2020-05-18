560aab1a8f19c227363f94468f63254bb2355aa3
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-05-18 19:59:01 +0200

    fix attribute

--- cmake/ifunc_test.c
+++ cmake/ifunc_test.c
@@ -1,3 +1,3 @@
-__attribute__((xtarget_clones("avx","sse4.2","sse3","sse2","default")))
+__attribute__((target_clones("avx","sse4.2","sse3","sse2","default")))
 void ifunc_test() {}
 int main(int argc, char** argv) {}
