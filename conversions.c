ef355e579eeb9c987c3d281889a202b361e036dd
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-11-21 08:01:12 +0100

    explicitly specify precision

--- conversions.c
+++ conversions.c
@@ -5,7 +5,7 @@ OWRX_CONNECTOR_TARGET_CLONES
 void convert_u8_f32(uint8_t* restrict input, float* restrict out, uint32_t count) {
     uint32_t i;
     for (i = 0; i < count; i++) {
-        out[i] = ((float) (input[i])) / (UINT8_MAX / 2.0) - 1.0;
+        out[i] = ((float) (input[i])) / (UINT8_MAX / 2.0f) - 1.0f;
     }
 }
 
@@ -21,7 +21,7 @@ OWRX_CONNECTOR_TARGET_CLONES
 void convert_s16_u8(int16_t* restrict input, uint8_t* restrict output, uint32_t count) {
     uint32_t i;
     for (i = 0; i < count; i++) {
-        output[i] = input[i] / 32767.0 * 128.0 + 127.4;
+        output[i] = input[i] / 32767.0f * 128.0f + 127.4f;
     }
 }
 
@@ -29,6 +29,6 @@ OWRX_CONNECTOR_TARGET_CLONES
 void convert_f32_u8(float* restrict in, uint8_t* restrict out, uint32_t count) {
     uint32_t i;
     for (i = 0; i < count; i++) {
-        out[i] = in[i] * UCHAR_MAX * 0.5 + 128;
+        out[i] = in[i] * UCHAR_MAX * 0.5f + 128;
     }
 }
