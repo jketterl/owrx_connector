0da67f3424301733772860b7a9a9d499f2de428e
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-11-29 09:55:17 +0100

    tuner type must not be 0

--- src/lib/rtl_tcp_connection.cpp
+++ src/lib/rtl_tcp_connection.cpp
@@ -17,7 +17,7 @@ void RtlTcpSocket::startNewConnection(int client_sock) {
 void RtlTcpConnection::sendHeaders() {
     dongle_info_t dongle_info;
     memcpy(&dongle_info.magic, "RTL0", 4);
-    dongle_info.tuner_type = 0; // unknown
+    dongle_info.tuner_type = 5; // R820T
     dongle_info.tuner_gain_count = 0;
     send(sock, (const char *)&dongle_info, sizeof(dongle_info), MSG_NOSIGNAL);
 }
